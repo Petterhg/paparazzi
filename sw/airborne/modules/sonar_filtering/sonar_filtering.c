b7dfa06f4531c2285246701d6bb16e2aaaa0635b
mkisantal <kisantal.matex@gmail.com>
2016-03-18 14:04:05 +0100

    file logger module created, not written, flight speed limited in the flight plan

---
+++ sw/airborne/modules/sonar_filtering/sonar_filtering.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) M. Kisantal
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/sonar_filtering/sonar_filtering.c"
+ * @author M. Kisantal
+ * filter the sonar the recorded sonar data
+ */
+
+#include "modules/sonar_filtering/sonar_filtering.h"
+
+// void sonar_filter_fcn {}
+
+
---
+++ sw/airborne/modules/sonar_filtering/sonar_filtering.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) M. Kisantal
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/sonar_filtering/sonar_filtering.h"
+ * @author M. Kisantal
+ * filter the sonar the recorded sonar data
+ */
+
+#ifndef SONAR_FILTERING_H
+#define SONAR_FILTERING_H
+
+// extern void sonar_filter_fcn;
+
+#endif
+
