02ae241c137c9cc53e81ade12398d60de6cccc7e
mkisantal <kisantal.matex@gmail.com>
2016-03-25 08:58:48 +0100

    First step of sonar mapping implementation

---
+++ sw/airborne/modules/sonar_mapping/sonar_mapping.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) M. Kisantal
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/sonar_mapping/sonar_mapping.c"
+ * @author M. Kisantal
+ * Module for processing logged data into information about cyberzoo sectors.
+ */
+
+#include "modules/sonar_mapping/sonar_mapping.h"
+
+// void process_log {}
+
+
---
+++ sw/airborne/modules/sonar_mapping/sonar_mapping.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) M. Kisantal
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/sonar_mapping/sonar_mapping.h"
+ * @author M. Kisantal
+ * Module for processing logged data into information about cyberzoo sectors.
+ */
+
+#ifndef SONAR_MAPPING_H
+#define SONAR_MAPPING_H
+
+// extern void process_log;
+
+#endif
+
